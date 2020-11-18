6f728f82dfb8e8fe30ceab002a7485901eac223d
Sandra Périvier <sandraperivier@MacBook-Air-de-Sandra.home>
2020-11-18 20:15:28 +0100

    first commit

---
+++ details.js
@@ -0,0 +1,31 @@
+const total.nb = [
+  {
+    id: 1,
+    pseudo: "@nathanf",
+    nb: "1987",
+    community: "Followers",
+    variation: "12 today"
+  },
+  {
+    id: 2,
+    pseudo: "@nathanf",
+    nb: "1044",
+    community: "Followers",
+    variation: "99 today"
+  },
+  {
+    id: 3,
+    pseudo: "@realnathanf",
+    nb: "11k",
+    community: "Followers",
+    variation: "1099 today"
+  },
+  {
+    id: 3,
+    pseudo: "Nathan F.",
+    nb: "8239",
+    community: "Subscribers",
+    variation: "144 today"
+  },
+
+];
---
+++ index.js
@@ -0,0 +1,5 @@
+let button = document.querySelector("input")
+
+button.addEventListener("click", ()=>{
+  document.documentElement.classList.toggle("dark-mode");
+})
